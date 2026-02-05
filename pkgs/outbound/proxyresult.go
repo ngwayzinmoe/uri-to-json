915c815a23afb59bcbfb6b3532b74226fcfa9ab0
ngwayzinmoe <32437857+ngwayzinmoe@users.noreply.github.com>
2026-02-05 21:26:31 +0630

    Update proxyresult.go

--- pkgs/outbound/proxyresult.go
+++ pkgs/outbound/proxyresult.go
@@ -6,8 +6,8 @@ import (
 
 	"encoding/json"
 
-	"github.com/dev7dev/uri-to-json/pkgs/parser"
-	"github.com/dev7dev/uri-to-json/pkgs/utils"
+	"github.com/ngwayzinmoe/uri-to-json/pkgs/parser"
+	"github.com/ngwayzinmoe/uri-to-json/pkgs/utils"
 	"github.com/gvcgo/goutils/pkgs/gutils"
 )
 
