c88a0ec8bfdc7295918320759553e462b4818243
ngwayzinmoe <32437857+ngwayzinmoe@users.noreply.github.com>
2026-02-06 22:29:59 +0630

    Update out_ss.go

--- pkgs/outbound/xray/out_ss.go
+++ pkgs/outbound/xray/out_ss.go
@@ -42,19 +42,16 @@ UDP over TCP 的实现版本。
 
 */
 
-var XraySS = `{
+var XraySS string = `{
 	"servers": [
-		{
-			"address": "",
-			"port": 0,
-			"method": "",
-			"password": "",
-			"uot": true,
-			"UoTVersion": 2
-		}
+	  {
+		"address": "127.0.0.1",
+		"port": 1234,
+		"method": "加密方式",
+		"password": "密码"
+	  }
 	]
 }`
-
 
 type ShadowSocksOut struct {
 	RawUri   string
@@ -62,97 +59,75 @@ type ShadowSocksOut struct {
 	outbound string
 }
 
-func (s *ShadowSocksOut) Parse(rawUri string) {
-	s.RawUri = rawUri
-	s.Parser = &parser.ParserSS{}
-	_ = s.Parser.Parse(rawUri) // silent-fail handled later
+func (that *ShadowSocksOut) Parse(rawUri string) {
+	that.Parser = &parser.ParserSS{}
+	that.Parser.Parse(rawUri)
 }
 
-func (s *ShadowSocksOut) Addr() string {
-	if s.Parser == nil {
+func (that *ShadowSocksOut) Addr() string {
+	if that.Parser == nil {
 		return ""
 	}
-	return s.Parser.Address
+	return that.Parser.GetAddr()
 }
 
-func (s *ShadowSocksOut) Port() int {
-	if s.Parser == nil {
+func (that *ShadowSocksOut) Port() int {
+	if that.Parser == nil {
 		return 0
 	}
-	return s.Parser.Port
+	return that.Parser.GetPort()
 }
 
-func (s *ShadowSocksOut) Scheme() string {
+func (that *ShadowSocksOut) Scheme() string {
 	return parser.SchemeSS
 }
 
-func (s *ShadowSocksOut) GetRawUri() string {
-	return s.RawUri
+func (that *ShadowSocksOut) GetRawUri() string {
+	return that.RawUri
 }
 
-func (s *ShadowSocksOut) getSettings() string {
-	if s.Parser == nil {
-		return ""
-	}
-
-	j := gjson.New(XraySS)
-
-	j.Set("servers.0.address", s.Parser.Address)
-	j.Set("servers.0.port", s.Parser.Port)
-	j.Set("servers.0.method", s.Parser.Method)
-	j.Set("servers.0.password", s.Parser.Password)
-
-	// UDP over TCP (Xray recommended)
-	j.Set("servers.0.uot", true)
-	j.Set("servers.0.UoTVersion", 2)
-
-	return j.MustToJsonString()
+func (that *ShadowSocksOut) getSettings() string {
+    j := gjson.New(XraySS)
+    j.Set("servers.0.address", that.Parser.Address)
+    j.Set("servers.0.port", that.Parser.Port)
+    j.Set("servers.0.method", that.Parser.Method)
+    j.Set("servers.0.password", that.Parser.Password)
+    
+    // UDP support အတွက် uot ကို true
+    j.Set("servers.0.uot", true) 
+    j.Set("servers.0.UoTVersion", 2)
+    return j.MustToJsonString()
 }
 
 
-func (s *ShadowSocksOut) setProtocolAndTag(outStr string) string {
+func (that *ShadowSocksOut) setProtocolAndTag(outStr string) string {
 	j := gjson.New(outStr)
 	j.Set("protocol", "shadowsocks")
 	j.Set("tag", utils.OutboundTag)
 	return j.MustToJsonString()
 }
 
-func (s *ShadowSocksOut) GetOutboundStr() string {
-	if s.Parser == nil {
+func (that *ShadowSocksOut) GetOutboundStr() string {
+	if that.Parser.Address == "" && that.Parser.Port == 0 {
 		return ""
 	}
-
-	// invalid ss
-	if s.Parser.Address == "" || s.Parser.Port <= 0 {
-		return ""
+	if that.outbound == "" {
+		settings := that.getSettings()
+		stream := PrepareStreamString(that.Parser.StreamField)
+		outStr := fmt.Sprintf(XrayOut, settings, stream)
+		that.outbound = that.setProtocolAndTag(outStr)
 	}
-
-	if s.outbound != "" {
-		return s.outbound
-	}
-
-	settings := s.getSettings()
-
-	// Shadowsocks does NOT use stream settings
-	outStr := fmt.Sprintf(XrayOut, settings, "")
-
-	s.outbound = s.setProtocolAndTag(outStr)
-	return s.outbound
+	return that.outbound
 }
 
 func TestSS() {
-	rawUri := "ss://aes-256-gcm:bad5fba5-a7bc-4709-882b-e15edad16cef@ah-cmi-1000m.ikun666.club:18878#CN-SG"
-
+	rawUri := "ss://aes-256-gcm:bad5fba5-a7bc-4709-882b-e15edad16cef@ah-cmi-1000m.ikun666.club:18878#🇨🇳_CN_中国-\u003e🇸🇬_SG_新加坡"
+	// rawUri := "ss://aes-128-gcm:g12sQi#ss#\u00261@183.232.170.32:20013?plugin=v2ray-plugin\u0026mode=websocket\u0026mux=undefined#🇨🇳_CN_中国-\u003e🇯🇵_JP_日本"
+	// rawUri := "ss://chacha20-ietf-poly1305:t0srmdxrm3xyjnvqz9ewlxb2myq7rjuv@4e168c3.h4.gladns.com:2377/?plugin=obfs-local\u0026obfs=tls\u0026obfs-host=(TG@WangCai_1)a83679f:53325#8DKJ|@Zyw_Channel"
 	sso := &ShadowSocksOut{}
 	sso.Parse(rawUri)
-
-	out := sso.GetOutboundStr()
-	if out == "" {
-		fmt.Println("invalid ss outbound")
-		return
-	}
-
-	j := gjson.New(out)
+	o := sso.GetOutboundStr()
+	j := gjson.New(o)
 	fmt.Println(j.MustToJsonIndentString())
+	fmt.Println(o)
 }
-
