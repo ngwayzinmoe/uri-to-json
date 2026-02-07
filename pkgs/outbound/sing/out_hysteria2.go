ae8e9584e890a331a4f355a16e879fc743fec81f
ngwayzinmoe <32437857+ngwayzinmoe@users.noreply.github.com>
2026-02-07 10:56:10 +0630

    Update out_hysteria2.go

--- pkgs/outbound/sing/out_hysteria2.go
+++ pkgs/outbound/sing/out_hysteria2.go
@@ -1,30 +1,10 @@
 package sing
 
 import (
+	"github.com/gogf/gf/v2/encoding/gjson"
 	"github.com/ngwayzinmoe/uri-to-json/pkgs/parser"
 	"github.com/ngwayzinmoe/uri-to-json/pkgs/utils"
-	"github.com/gogf/gf/v2/encoding/gjson"
 )
-
-/*
-Sing-box Hysteria2 Configuration:
-{
-  "type": "hysteria2",
-  "tag": "hysteria2-out",
-  "server": "127.0.0.1",
-  "server_port": 443,
-  "password": "auth_password",
-  "tls": {
-    "enabled": true,
-    "server_name": "example.com",
-    "insecure": false
-  },
-  "obfs": {
-    "type": "salamander",
-    "password": "obfs_password"
-  }
-}
-*/
 
 type SHysteria2Out struct {
 	RawUri   string
@@ -38,56 +18,49 @@ func (that *SHysteria2Out) Parse(rawUri string) {
 	that.Parser.Parse(rawUri)
 }
 
-func (that *SHysteria2Out) Addr() string {
-	return that.Parser.Config.Server
-}
-
-func (that *SHysteria2Out) Port() int {
-	return that.Parser.Config.Port
-}
-
-func (that *SHysteria2Out) Scheme() string {
-	return parser.SchemeHysteria2
-}
-
-func (that *SHysteria2Out) GetRawUri() string {
-	return that.RawUri
-}
+func (that *SHysteria2Out) Addr() string { return that.Parser.GetAddr() }
+func (that *SHysteria2Out) Port() int    { return that.Parser.GetPort() }
 
 func (that *SHysteria2Out) GetOutboundStr() string {
 	if that.Parser.Config.Server == "" {
 		return ""
 	}
-
 	if that.outbound == "" {
 		j := gjson.New("{}")
 		j.Set("type", "hysteria2")
 		j.Set("tag", utils.OutboundTag)
 		j.Set("server", that.Parser.Config.Server)
 		j.Set("server_port", that.Parser.Config.Port)
-		
-		// Hysteria2 main password (auth)
 		j.Set("password", that.Parser.Config.Auth)
 
-		// TLS settings
-		j.Set("tls.enabled", true)
-		if that.Parser.Config.SNI != "" {
-			j.Set("tls.server_name", that.Parser.Config.SNI)
+		// BDP (Bandwidth Control)
+		if that.Parser.Config.UpMbps > 0 {
+			j.Set("up_mbps", that.Parser.Config.UpMbps)
 		}
-		if that.Parser.Config.Insecure {
-			j.Set("tls.insecure", true)
+		if that.Parser.Config.DownMbps > 0 {
+			j.Set("down_mbps", that.Parser.Config.DownMbps)
 		}
 
-		// OBFS settings
+		// TLS Object
+		tlsConf := map[string]interface{}{
+			"enabled":     true,
+			"server_name": that.Parser.Config.SNI,
+			"insecure":    that.Parser.Config.Insecure,
+		}
+		j.Set("tls", tlsConf)
+
+		// OBFS Object (Salamander support)
 		if that.Parser.Config.OBFS != "" {
-			j.Set("obfs.type", that.Parser.Config.OBFS)
-			if that.Parser.Config.OBFSPass != "" {
-				j.Set("obfs.password", that.Parser.Config.OBFSPass)
+			obfsConf := map[string]interface{}{
+				"type": that.Parser.Config.OBFS,
 			}
+			if that.Parser.Config.OBFSPass != "" {
+				obfsConf["password"] = that.Parser.Config.OBFSPass
+			}
+			j.Set("obfs", obfsConf)
 		}
 
 		that.outbound = j.MustToJsonString()
 	}
 	return that.outbound
 }
-
