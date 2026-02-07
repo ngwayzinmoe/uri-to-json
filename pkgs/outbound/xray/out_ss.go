8108b0360e85d4c5380e2d85d82a9f26352f3cc9
ngwayzinmoe <32437857+ngwayzinmoe@users.noreply.github.com>
2026-02-07 12:15:40 +0630

    Update out_ss.go

--- pkgs/outbound/xray/out_ss.go
+++ pkgs/outbound/xray/out_ss.go
@@ -2,56 +2,10 @@ package xray
 
 import (
 	"fmt"
-
 	"github.com/ngwayzinmoe/uri-to-json/pkgs/parser"
 	"github.com/ngwayzinmoe/uri-to-json/pkgs/utils"
 	"github.com/gogf/gf/v2/encoding/gjson"
 )
-
-/*
-https://xtls.github.io/config/outbounds/shadowsocks.html#serverobject
-
-{
-	"servers": [
-	  {
-		"email": "love@xray.com",
-		"address": "127.0.0.1",
-		"port": 1234,
-		"method": "加密方式",
-		"password": "密码",
-		"uot": true,
-		"UoTVersion": 2,
-		"level": 0
-	  }
-	]
-}
-
-Method:
-2022-blake3-aes-128-gcm
-2022-blake3-aes-256-gcm
-2022-blake3-chacha20-poly1305
-aes-256-gcm
-aes-128-gcm
-chacha20-poly1305 或称 chacha20-ietf-poly1305
-xchacha20-poly1305 或称 xchacha20-ietf-poly1305
-none 或 plain
-
-UoTVersion:
-UDP over TCP 的实现版本。
-当前可选值：1, 2
-
-*/
-
-var XraySS string = `{
-	"servers": [
-	  {
-		"address": "127.0.0.1",
-		"port": 1234,
-		"method": "加密方式",
-		"password": "密码"
-	  }
-	]
-}`
 
 type ShadowSocksOut struct {
 	RawUri   string
@@ -60,62 +14,39 @@ type ShadowSocksOut struct {
 }
 
 func (that *ShadowSocksOut) Parse(rawUri string) {
+	that.RawUri = rawUri
 	that.Parser = &parser.ParserSS{}
 	that.Parser.Parse(rawUri)
 }
 
-func (that *ShadowSocksOut) Addr() string {
-	if that.Parser == nil {
-		return ""
-	}
-	return that.Parser.GetAddr()
-}
-
-func (that *ShadowSocksOut) Port() int {
-	if that.Parser == nil {
-		return 0
-	}
-	return that.Parser.GetPort()
-}
-
-func (that *ShadowSocksOut) Scheme() string {
-	return parser.SchemeSS
-}
-
-func (that *ShadowSocksOut) GetRawUri() string {
-	return that.RawUri
-}
-
-func (that *ShadowSocksOut) getSettings() string {
-    j := gjson.New(XraySS)
-    j.Set("servers.0.address", that.Parser.Address)
-    j.Set("servers.0.port", that.Parser.Port)
-    j.Set("servers.0.method", that.Parser.Method)
-    j.Set("servers.0.password", that.Parser.Password)
-    
-    // UDP support အတွက် uot ကို true
-    j.Set("servers.0.uot", true) 
-    j.Set("servers.0.UoTVersion", 2)
-    return j.MustToJsonString()
-}
-
-
-func (that *ShadowSocksOut) setProtocolAndTag(outStr string) string {
-	j := gjson.New(outStr)
-	j.Set("protocol", "shadowsocks")
-	j.Set("tag", utils.OutboundTag)
-	return j.MustToJsonString()
-}
-
 func (that *ShadowSocksOut) GetOutboundStr() string {
-	if that.Parser.Address == "" && that.Parser.Port == 0 {
-		return ""
-	}
+	if that.Parser.Address == "" { return "" }
+	
 	if that.outbound == "" {
-		settings := that.getSettings()
+		// Servers Setting
+		server := map[string]interface{}{
+			"address":  that.Parser.Address,
+			"port":     that.Parser.Port,
+			"method":   that.Parser.Method,
+			"password": that.Parser.Password,
+		}
+		
+		// [UoT for Xray]
+		if that.Parser.StreamField.UoT {
+			server["uot"] = true
+			server["UoTVersion"] = 2
+		}
+
+		j := gjson.New("{}")
+		j.Set("protocol", "shadowsocks")
+		j.Set("tag", utils.OutboundTag)
+		j.Set("settings.servers", []interface{}{server})
+		
+		// Stream Settings
 		stream := PrepareStreamString(that.Parser.StreamField)
-		outStr := fmt.Sprintf(XrayOut, settings, stream)
-		that.outbound = that.setProtocolAndTag(outStr)
+		j.Set("streamSettings", gjson.New(stream).Map())
+		
+		that.outbound = j.MustToJsonString()
 	}
 	return that.outbound
 }
