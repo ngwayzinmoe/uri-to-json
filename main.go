4397b66f476735c48626ffa18664d7b48ae1afe0
ngwayzinmoe <32437857+ngwayzinmoe@users.noreply.github.com>
2026-02-09 01:28:49 +0630

    Update main.go

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/ngwayzinmoe/uri-to-json/pkgs/cmd"
-	_ "github.com/ngwayzinmoe/uri-to-json/pkgs/outbound/sing"
 	_ "github.com/ngwayzinmoe/uri-to-json/pkgs/outbound/xray"
 	_ "github.com/ngwayzinmoe/uri-to-json/pkgs/parser"
 )
