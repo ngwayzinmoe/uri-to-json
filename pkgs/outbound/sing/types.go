0c5566a00c4e752d458a702616a7870459b69961
ngwayzinmoe <32437857+ngwayzinmoe@users.noreply.github.com>
2026-02-09 01:17:20 +0630

    Delete pkgs/outbound/sing directory

--- pkgs/outbound/sing/types.go
+++
@@ -1,9 +0,0 @@
-package sing
-
-import "github.com/ngwayzinmoe/uri-to-json/pkgs/parser"
-
-// Common sing-box Shadowsocks outbound
-type SingSS struct {
-	ParserSS  *parser.ParserSS
-	ParserSSR *parser.ParserSSR
-}
